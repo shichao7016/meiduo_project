9a067c738128ccfccec8241a9954222605cea14f
xwp <xwp_fullstack@163.com>
2019-09-15 17:15:52 +0800

    第一阶段

---
+++ meiduo_mall/apps/users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class RegisterView(View):
+
+
+    def get(self, request):
+
+        return render(request, 'register.html')
